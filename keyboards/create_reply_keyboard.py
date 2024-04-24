ed7e2c2e695a8c6b060888841f482c6a6fcada52
ALK398 <154527213+ALK398@users.noreply.github.com>
2024-04-24 21:22:07 +0300

    fix

--- keyboards/create_reply_keyboard.py
+++ keyboards/create_reply_keyboard.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 from lexicon.lexicon_buttons import LEXICON_BUTTONS
@@ -16,23 +15,4 @@ def create_reply_kb(width: int, *args: str, one_time_keyboard: bool = True) -> R
     kb_builder.row(*buttons, width=width)
 
     return kb_builder.as_markup(resize_keyboard=True,
-=======
-from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
-from lexicon.lexicon_buttons import LEXICON_BUTTONS
-
-
-def create_reply_kb(width: int, *args: str, one_time_keyboard: bool = True) -> ReplyKeyboardMarkup:
-    kb_builder: ReplyKeyboardBuilder = ReplyKeyboardBuilder()
-    buttons: list[KeyboardButton] = []
-
-    if args:
-        for button in args:
-            buttons.append(KeyboardButton(
-                text=LEXICON_BUTTONS[button] if button in LEXICON_BUTTONS else button))
-
-    kb_builder.row(*buttons, width=width)
-
-    return kb_builder.as_markup(resize_keyboard=True,
->>>>>>> origin/master
                                 one_time_keyboard=one_time_keyboard)
